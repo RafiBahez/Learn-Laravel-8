9c0a41862da4edf0ca991f7a0e2234173dc3db17
= <=>
2020-11-14 18:47:09 +0430

    Middleware Check Age Example

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->age <=21){
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
---
+++ resources/views/news.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>Laravel</title>
+
+    </head>
+    <body class="bg-gray-300 container mx-auto py-20">
+    <h2>Only allowed for 21+ years.</h2>
+    </body>
+</html>
